34da2cf1edd8f9f3cbdfb8cd21366f72e7308bbc
Hannan Rafiq <165180747+Hacker00022@users.noreply.github.com>
2024-09-26 20:43:04 +0500

    Add files via upload

---
+++ images/script.js
@@ -0,0 +1,6 @@
+// You can add any interactivity with JavaScript here if needed
+
+// Example: Scroll to top of the page on button click
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('Page Loaded! You can add interactivity here.');
+});
